2d1a066840de7f1355423460b1076b4796bf06e4
aroquev00 <aroquev@google.com>
2020-06-26 22:39:06 +0000

    Add car info containers in HTML

--- aroquev/src/main/webapp/script.js
+++ aroquev/src/main/webapp/script.js
@@ -15,58 +15,73 @@
 /**
  * Adds a random car and its data to the page.
  */
-function addRandomCar() {
-	const cars = {
-		mclaren: {
+function getRandomCar() {
+	const cars = [
+		{
 			name: "McLaren 720S",
 			power: "720 hp",
 			country: "England",
 			funFact: "Just look at it, it is beautiful!"
 		},
-		porsche: {
+		{
 			name: "Porsche 918 Spyder",
 			power: "720 hp",
 			country: "Germany",
 			funFact: "Hybrid power for the win!"
 		},
-		ferrari: {
+		{
 			name: "Ferrari FXX Evoluzione",
 			power: "848 hp",
 			country: "Italy",
 			funFact: "You should really listen to the sound this one makes"
 		},
-		lamborghini: {
+		{
 			name: "Lamborghini Sesto Elemento",
 			power: "562 hp",
 			country: "Italy",
 			funFact: "The whole thing weights less that a tonne thanks to carbon fiber!"
 		},
-		mercedes: {
+		{
 			name: "Mercedes-AMG G63 6x6",
 			power: "536 hp",
 			country: "Germany",
 			funFact: "Taking offroading to a whole new level."
 		},
-		subaru: {
+		{
 			name: "Subaru WRX STI",
 			power: "536 hp",
 			country: "Japan",
 			funFact: "Boxster engines rock!"
 		},
-		mini: {
+		{
 			name: "Mini JCW",
 			power: "306 hp",
 			country: "England",
 			funFact: "I currently own a Mini :)."
 		},
-		vw: {
+		{
 			name: "Volkswagen Sedan",
 			power: "Unlimited hp",
 			country: "Germany",
 			funFact: "Grandpa's car <3, learned to drive in it."
 		}
-	};
+    ];
 
+    // Pick a random car.
+    const car = cars[Math.floor(Math.random() * cars.length)];
+
+    // Add it to the page.
+    const carNameContainer = document.getElementById('car-name-container');
+    carNameContainer.innerText = "Name: " + car.name;
+
+    const carPowerContainer = document.getElementById('car-power-container');
+    carPowerContainer.innerText = "Power: " + car.power;
+
+    const carCountryContainer = document.getElementById('car-country-container');
+    carCountryContainer.innerText = "Country: " + car.country;
+
+    const carFunFactContainer = document.getElementById('car-funFact-container');
+    carFunFactContainer.innerText = "Why I like this car? " + car.funFact;
 }
 
 /**
