84e2227fcebd3ba10e2c3f20ab24df3234b8fa66
aroquev00 <aroquev@google.com>
2020-06-29 22:11:12 -0500

    Fix indentation and add deployment in .md

--- aroquev/README.md
+++ aroquev/README.md
@@ -2,6 +2,8 @@
 
 This repo contains Armando Roque's portfolio and STEP projects. :fire: :fire: :fire:
 
+Link to deployment: https://aroquev-step-2020.uc.r.appspot.com/ :rocket:
+
 By default it contains a barebones web app. To run a local server, execute this
 command:
 
--- aroquev/src/main/webapp/script.js
+++ aroquev/src/main/webapp/script.js
@@ -16,28 +16,28 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-	const greetings =
-		['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+  const greetings =
+    ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-	// Pick a random greeting.
-	const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random greeting.
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-	// Add it to the page.
-	const greetingContainer = document.getElementById('js-greeting-container');
-	greetingContainer.innerText = greeting;
+  // Add it to the page.
+  const greetingContainer = document.getElementById('js-greeting-container');
+  greetingContainer.innerText = greeting;
 }
 
 /**
  * Adds Armandos age to the page.
  */
 function calculateArmandoAge() {
-	// Calculate age, Armando was born on May 13, 2000.
-	const diffMs = new Date() - new Date(2000, 5, 13);
-	const ageDt = new Date(diffMs);
+  // Calculate age, Armando was born on May 13, 2000.
+  const diffMs = new Date() - new Date(2000, 5, 13);
+  const ageDt = new Date(diffMs);
 
-	// Add it to the page.
-	const ageContainer = document.getElementById('armando-age-container');
-	ageContainer.innerText = Math.abs(ageDt.getUTCFullYear() - 1970);
+  // Add it to the page.
+  const ageContainer = document.getElementById('armando-age-container');
+  ageContainer.innerText = Math.abs(ageDt.getUTCFullYear() - 1970);
 
 }
 
@@ -45,42 +45,42 @@ function calculateArmandoAge() {
 var slideIndex = 0;
 
 function initSlides() {
-	showSlides(slideIndex);
+  showSlides(slideIndex);
 }
 
 // Next control
 function showNextSlide() {
-	slideIndex++;
-	showSlides(slideIndex);
+  slideIndex++;
+  showSlides(slideIndex);
 }
 
 // Previous control
 function showPreviousSlide() {
-	slideIndex--;
-	showSlides(slideIndex);
+  slideIndex--;
+  showSlides(slideIndex);
 }
 
 // Thumbnail image controls
 function updateCurrentSlide(n) {
-	slideIndex = n;
-	showSlides(slideIndex);
+  slideIndex = n;
+  showSlides(slideIndex);
 }
 
 function showSlides(n) {
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot");
-	slideIndex = n % slides.length;
-	if (slideIndex == -1) {
-		slideIndex = slides.length - 1;
-	}
-	for (const slide of slides) {
-		slide.style.display = "none";
-	}
-	for (const dot of dots) {
-		dot.classList.remove("active");
-	}
-	slides[slideIndex].style.display = "block";
-	dots[slideIndex].classList.add("active");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  slideIndex = n % slides.length;
+  if (slideIndex == -1) {
+    slideIndex = slides.length - 1;
+  }
+  for (const slide of slides) {
+    slide.style.display = "none";
+  }
+  for (const dot of dots) {
+    dot.classList.remove("active");
+  }
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].classList.add("active");
 }
 
 initSlides();
@@ -89,81 +89,81 @@ initSlides();
  * Adds a random car and its data to the page.
  */
 function getRandomCar() {
-	const cars = [
-		{
-			name: "McLaren 720S",
-			power: "720 hp",
-			country: "England",
-			funFact: "Just look at it, it is beautiful!",
-			path: "images/720s.jpg"
-		},
-		{
-			name: "Porsche 918 Spyder",
-			power: "720 hp",
-			country: "Germany",
-			funFact: "Hybrid power for the win!",
-			path: "images/porsche.jpeg"
-		},
-		{
-			name: "Ferrari FXX Evoluzione",
-			power: "848 hp",
-			country: "Italy",
-			funFact: "You should really listen to the sound this one makes",
-			path: "images/ferrari.jpg"
-		},
-		{
-			name: "Lamborghini Sesto Elemento",
-			power: "562 hp",
-			country: "Italy",
-			funFact: "The whole thing weights less that a tonne thanks to carbon fiber!",
-			path: "images/lambo.jpg"
-		},
-		{
-			name: "Mercedes-AMG G63 6x6",
-			power: "536 hp",
-			country: "Germany",
-			funFact: "Taking offroading to a whole new level.",
-			path: "images/mercedes.jpeg"
-		},
-		{
-			name: "Subaru WRX STI",
-			power: "536 hp",
-			country: "Japan",
-			funFact: "Boxster engines rock!",
-			path: "images/subaru.jpg"
-		},
-		{
-			name: "Mini JCW",
-			power: "306 hp",
-			country: "England",
-			funFact: "I currently own a Mini :).",
-			path: "images/mini.jpeg"
-		},
-		{
-			name: "Volkswagen Sedan",
-			power: "Unlimited hp",
-			country: "Germany",
-			funFact: "Grandpa's car <3, learned to drive in it.",
-			path: "images/vw.jpg"
-		}
-	];
+  const cars = [
+    {
+      name: "McLaren 720S",
+      power: "720 hp",
+      country: "England",
+      funFact: "Just look at it, it is beautiful!",
+      path: "images/720s.jpg"
+    },
+    {
+      name: "Porsche 918 Spyder",
+      power: "720 hp",
+      country: "Germany",
+      funFact: "Hybrid power for the win!",
+      path: "images/porsche.jpeg"
+    },
+    {
+      name: "Ferrari FXX Evoluzione",
+      power: "848 hp",
+      country: "Italy",
+      funFact: "You should really listen to the sound this one makes",
+      path: "images/ferrari.jpg"
+    },
+    {
+      name: "Lamborghini Sesto Elemento",
+      power: "562 hp",
+      country: "Italy",
+      funFact: "The whole thing weights less that a tonne thanks to carbon fiber!",
+      path: "images/lambo.jpg"
+    },
+    {
+      name: "Mercedes-AMG G63 6x6",
+      power: "536 hp",
+      country: "Germany",
+      funFact: "Taking offroading to a whole new level.",
+      path: "images/mercedes.jpeg"
+    },
+    {
+      name: "Subaru WRX STI",
+      power: "536 hp",
+      country: "Japan",
+      funFact: "Boxster engines rock!",
+      path: "images/subaru.jpg"
+    },
+    {
+      name: "Mini JCW",
+      power: "306 hp",
+      country: "England",
+      funFact: "I currently own a Mini :).",
+      path: "images/mini.jpeg"
+    },
+    {
+      name: "Volkswagen Sedan",
+      power: "Unlimited hp",
+      country: "Germany",
+      funFact: "Grandpa's car <3, learned to drive in it.",
+      path: "images/vw.jpg"
+    }
+  ];
 
-	// Pick a random car.
-	const car = cars[Math.floor(Math.random() * cars.length)];
+  // Pick a random car.
+  const car = cars[Math.floor(Math.random() * cars.length)];
 
-	// Add it to the page.
-	const carImageContainer = document.getElementById('js-car-image');
-	carImageContainer.src = car.path;
+  // Add it to the page.
+  const carImageContainer = document.getElementById('js-car-image');
+  carImageContainer.src = car.path;
 
-	const carNameContainer = document.getElementById('js-car-name-container');
-	carNameContainer.innerText = "Name: " + car.name;
+  const carNameContainer = document.getElementById('js-car-name-container');
+  carNameContainer.innerText = "Name: " + car.name;
 
-	const carPowerContainer = document.getElementById('js-car-power-container');
-	carPowerContainer.innerText = "Power: " + car.power;
+  const carPowerContainer = document.getElementById('js-car-power-container');
+  carPowerContainer.innerText = "Power: " + car.power;
 
-	const carCountryContainer = document.getElementById('js-car-country-container');
-	carCountryContainer.innerText = "Country: " + car.country;
+  const carCountryContainer = document.getElementById('js-car-country-container');
+  carCountryContainer.innerText = "Country: " + car.country;
 
-	const carFunFactContainer = document.getElementById('js-car-funFact-container');
-	carFunFactContainer.innerText = "Why I like this car? " + car.funFact;
+  const carFunFactContainer = document.getElementById('js-car-funFact-container');
+  carFunFactContainer.innerText = "Why I like this car? " + car.funFact;
 }
