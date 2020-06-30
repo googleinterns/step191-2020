02d9c6bebb96f602973adf4e048fec38045f89a0
Pamela <pamelalozano@google.com>
2020-06-30 17:02:53 +0000

    Add date attribute

--- pamelalozano/src/main/java/com/google/sps/servlets/Comment.java
+++ pamelalozano/src/main/java/com/google/sps/servlets/Comment.java
@@ -10,22 +10,30 @@ public class Comment {
   
   private String subject;
   private String msg;
+  private Date date;
 
-  public Comment(String s, String m) {
-    this.subject=s;
-    this.msg=m;
+  public Comment(String s, String m, Date d) {
+    this.subject = s;
+    this.msg = m;
+    this.date = d;
   }
   
-  public void setSubject(String s){
-      this.subject=s;
+  public void setSubject(String s) {
+      this.subject = s;
   }
-  public void setMessage(String m){
-      this.msg=m;
+  public void setMessage(String m) {
+      this.msg = m;
   }
-  public String getSubject(){
+  public void setDate(Date d) {
+      this.date = d;
+  }
+  public String getSubject() {
       return this.subject;
   }
-  public String getMessage(){
+  public String getMessage() {
       return this.msg;
+  }
+  public Date getDate() {
+      return this.date;
   }
 };
