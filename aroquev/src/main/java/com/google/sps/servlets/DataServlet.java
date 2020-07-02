29589edd870f924c75c69ec334c0c628c363bf76
aroquev00 <aroquev@google.com>
2020-07-02 12:07:23 -0500

    Update coments

--- aroquev/src/main/java/com/google/sps/servlets/DataServlet.java
+++ aroquev/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns some example content.*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet { 
 
@@ -42,7 +42,6 @@ public class DataServlet extends HttpServlet {
     final ArrayList<String> comments = new ArrayList<String>();
     for (Entity entity : results.asIterable()) {
       String comment = (String) entity.getProperty("commentBody");
-
       comments.add(comment);
     }
 
