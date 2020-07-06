6faf2ea5159ef1e8d21b2b58defa379021c9bd9f
Casale <popobasura3@hotmail.com>
2020-07-06 12:02:20 -0500

    Comments feature added (#40)
    * Comments can be added
    
    * Debug comments deteled
    
    * Comments can handle title, description and username
    
    * Comments styled
    
    * Delete comment function recieves id
    
    * Innecesary comment deleted
    
    * Functions added to create a comment and to create an entity
    
    * When a comment is deleted the website fetches the comments
    
    * Guava updated
    
    * Comments fetched after comments are deleted

---
+++ ayup/portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
@@ -0,0 +1,64 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Comment;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet responsible for listing comments. */
+@WebServlet("/list-comments")
+public class ListCommentsServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<Comment> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      Comment comment = entityToComment(entity);
+      comments.add(comment);
+    }
+
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+  }
+
+  private Comment entityToComment(Entity entity){
+    long id = entity.getKey().getId();
+    String title = (String) entity.getProperty("title");
+    String description = (String) entity.getProperty("description");
+    String username = (String) entity.getProperty("username");
+    long timestamp = (long) entity.getProperty("timestamp");
+    return new Comment(id, title, description, username, timestamp);
+  }
+}
