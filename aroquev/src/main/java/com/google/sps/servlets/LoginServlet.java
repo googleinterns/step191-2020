f7d5fefd5bfc10470eae6502fb2ed3a622c05378
Armando Roque <43384970+aroquev00@users.noreply.github.com>
2020-07-08 13:41:16 -0500

    Servlet returns User Login Status (#51)
    * Add datastore to store comments
    
    * Load comments from DS
    
    * Fix imports
    
    * Delete unused variables
    
    * Update coments
    
    * Show hardcoded number of comments
    
    * Add number of comments to be displayed
    
    * Modularize input validation
    
    * Make num. comments change onchange. Fix error message to disappear
    
    * Add delete all button
    
    * Add delete all comments functionality
    
    * Add comment class
    
    * Add placeholders in HTML textareas
    
    * Add votes and username to comments, uses hardcoded data
    
    * Add real data to votes in GET
    
    * try to deserialize json
    
    * Add votes functionality, missing number in view
    
    * Add votes message in comments
    
    * Add date format to Comments in HTML
    
    * Add single comment delete button
    
    * Add favicon
    
    * Add navbar
    
    * Add footer
    
    * Round images
    
    * Change typography
    
    * Add votes icons
    
    * Add styling to comments section
    
    * Add server that returns if user is logged in

---
+++ aroquev/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -0,0 +1,37 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login-status")
+public class LoginServlet extends HttpServlet { 
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    boolean isLoggedIn = userService.isUserLoggedIn();
+
+    response.getWriter().println(isLoggedIn);
+  }
+
+}
