c59fd480ca69bb0af7ff15a3f152c0f44292e7c5
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2020-07-09 12:28:01 -0500

    Log in and Log out in comment section (#57)
    * Create nickname for user
    
    Post nickname as author in comments
    
    * Toggle comment section buttons and box according to login status
    
    Show author in comments
    
    * Style changes
    
    * Refactor getUserId function with keys

--- pamelalozano/src/main/java/com/google/sps/servlets/Auth.java
+++ pamelalozano/src/main/java/com/google/sps/servlets/Auth.java
@@ -12,6 +12,7 @@ public class Auth {
   private final String logoutUrl;
   private final String loginUrl;
   private final String activeUser;
+  private final String nickname;
  
   public String getLoginUrl() {
       return this.loginUrl;
@@ -28,6 +29,9 @@ public class Auth {
   public Boolean isAdministrator() {
       return this.isAdmin;
   }
+  public String getNickname() {
+      return this.nickname;
+  }
 
   private Auth(Builder builder) {
     isLoggedIn = builder.isLoggedIn;
@@ -35,14 +39,16 @@ public class Auth {
     activeUser = builder.activeUser;
     loginUrl = builder.loginUrl;
     logoutUrl = builder.logoutUrl;
+    nickname = builder.nickname;
   }
 
   public static class Builder {
     private boolean isLoggedIn=false;
     private boolean isAdmin=false;
-    private String logoutUrl=null;
-    private String loginUrl=null;
-    private String activeUser=null;
+    private String logoutUrl;
+    private String loginUrl;
+    private String activeUser;
+    private String nickname;
 
     public Builder setLoginUrl(String loginUrl) {
       this.loginUrl = loginUrl;
@@ -62,6 +68,10 @@ public class Auth {
     }
     public Builder setActiveUser(String activeUser) {
       this.activeUser = activeUser;
+      return this;
+    }
+    public Builder setNickname(String nickname) {
+      this.nickname = nickname;
       return this;
     }
     public Auth build() {
