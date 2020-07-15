865772b2339ad79fd2fa6076d2a5d97e77a13569
Rodrigo <popobasura3@hotmail.com>
2020-07-15 17:21:44 -0500

    (Nitfix) Comments added to explain how FindMeetingQuery.java works

--- ayup/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ ayup/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -26,10 +26,16 @@ public final class FindMeetingQuery {
     Collection<TimeRange> returnQuery = new ArrayList<>();
     List<TimeRange> conflicts = new ArrayList<>();
 
+    // If the request is longer than one day we return an empty query.
     if (request.getDuration() > (24 * 60)) {
       return returnQuery;
     }
 
+    /**
+     * Check in every event if the attendees are in the requests.
+     * If true, we add the event to the conflicted set of events.
+     * Then we sert the conflicted time ranges by start.
+    */
     for (Event event: events) {
       if (!Collections.disjoint(request.getAttendees(), event.getAttendees())) {
         conflicts.add(event.getWhen());
@@ -38,14 +44,26 @@ public final class FindMeetingQuery {
 
     Collections.sort(conflicts, TimeRange.ORDER_BY_START);
 
+    /**
+     * If the are no events or if there are no conflicts, that means the whole day is free.
+     * We return a time range for the whole day.
+     */
     if (events.isEmpty() || conflicts.isEmpty()) {
       returnQuery.add(TimeRange.WHOLE_DAY);
       return returnQuery;
     } 
     
+    /**
+     * We go through all of the conflicts and if there is a time difference between the previous
+     * and current conflicts bitter than the duration of the request, we add to the query we are
+     * goint to return a time range between the ending of the previous event and the begining of
+     * the new event.
+     */
     TimeRange previous = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.START_OF_DAY, false);
 
     for (TimeRange current: conflicts) { 
+
+      // If the previous event cointains the current event we break the for loop
       if (previous.contains(current)) break;
 
       if (current.start() - previous.end() >= request.getDuration()) {
@@ -54,6 +72,7 @@ public final class FindMeetingQuery {
       previous = current;
     }
 
+    // If there's space, we add a time range between the ending of the last event to the end of the day.
     if (TimeRange.END_OF_DAY - previous.end() >= request.getDuration()) {
       returnQuery.add(TimeRange.fromStartEnd(previous.end(), TimeRange.END_OF_DAY, true));
     }
