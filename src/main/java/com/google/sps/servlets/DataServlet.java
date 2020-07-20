8291884303a030d5b9b02b2fc59c1816621fef92
aroquev00 <aroquev@google.com>
2020-07-20 20:47:14 +0000

    Try to upload Ada Lovlace info to Firestore

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,13 +14,27 @@
 
 package com.google.sps.servlets;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DatabaseReference;
+import com.google.api.core.ApiFuture;
 
 import com.google.auth.oauth2.GoogleCredentials;
 
+import com.google.cloud.firestore.DocumentReference;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+
 import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,7 +45,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private DatabaseReference firebase;
+  private Firestore db;
 
   @Override
   public void init() {
@@ -39,14 +53,28 @@ public class DataServlet extends HttpServlet {
     try {
       FirebaseOptions options = new FirebaseOptions.Builder()
     .setCredentials(GoogleCredentials.getApplicationDefault())
-    .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
+    .setDatabaseUrl("https://quizzy-step-2020.firebaseio.com/")
     .build();
 
       FirebaseApp.initializeApp(options);
       System.out.println("HIIII");
+
+      // [START fs_initialize_project_id]
+      FirestoreOptions firestoreOptions =
+      FirestoreOptions.getDefaultInstance().toBuilder()
+          .setProjectId("quizy-step-2020")
+          .setCredentials(GoogleCredentials.getApplicationDefault())
+          .build();
+      Firestore db = firestoreOptions.getService();
+      // [END fs_initialize_project_id]
+      this.db = db;
+
     } catch(IOException ie) {
       ie.printStackTrace();
+      System.out.println("FAIL");
     }
+
+    
     
     
     // FirebaseOptions options = new FirebaseOptions.Builder()
@@ -63,5 +91,23 @@ public class DataServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello world!</h1>");
+
+    DocumentReference docRef = db.collection("messages").document("alovelace");
+    Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+
+    //asynchronously write data
+    ApiFuture<WriteResult> result = docRef.set(data);
+    // ...
+    // result.get() blocks on response
+    try {
+      System.out.println("Update time : " + result.get().getUpdateTime());
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    
+
   }
 }
