04a18c55789bdf0e45d81d4bdee331de6fe5b0e4
Armando Roque <43384970+aroquev00@users.noreply.github.com>
2020-07-22 13:21:48 -0500

    Realtime counter in Servlet & Reallocate Rodrigo's TDD files (#78)
    * Move our starter files to their own folder
    
    * Add starter files for capstone
    
    * Add starter files for capstone
    
    * Add firebase dependency to POM.xml
    
    * Initialize Firebase SDK?
    
    * Try to upload Ada Lovlace info to Firestore
    
    * Manage to post a message from servlet
    
    * Post an object to Realtime Database
    
    * Comment writing to DBs
    
    * Manage to print counters?
    
    * Accurately logs info
    
    * Work with temp counter
    
    * Make autonomous counter work
    
    * Get addValueEventListener to work
    
    * Add callback for realtime DB
    
    * Put Rodrigo's files in his starter folder
    
    * Comment one setValue method

---
+++ src/main/java/com/google/sps/data/Counter.java
@@ -0,0 +1,22 @@
+package com.google.sps.data;
+
+public class Counter {
+
+  public int value;
+
+  public Counter() {
+  }
+
+  public Counter(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setCounter(int value) {
+    this.value = value;
+  }
+  
+}
