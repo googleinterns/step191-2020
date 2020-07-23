f55a1a3fe84a0c02577689dc3f9360b29f8041d2
aroquev00 <aroquev@google.com>
2020-07-23 16:44:47 +0000

    Initialize Firebase in context listener

--- src/main/java/com/google/sps/util/QuizzyContextListener.java
+++ src/main/java/com/google/sps/util/QuizzyContextListener.java
@@ -1,9 +1,21 @@
 package com.google.sps.util;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import com.google.sps.daos.CounterDao;
 import com.google.sps.daos.RealtimeDao;
 
 @WebListener("Context Listener")
@@ -17,10 +29,38 @@ public class QuizzyContextListener implements ServletContextListener {
     // This function is called when the application starts and will safely set a few required
     // context attributes such as the RealtimeDao
 
-    RealtimeDao dao = (RealtimeDao) event.getServletContext().getAttribute("dao");
+    FirebaseDatabase database = initializeRealtimeFirebase();
+
+    CounterDao dao = (CounterDao) event.getServletContext().getAttribute("dao");
     if (dao == null) {
-      dao = new RealtimeDao();
+      dao = new RealtimeDao(database);
       event.getServletContext().setAttribute("dao", dao);
     }
+  }
+
+  private FirebaseDatabase initializeRealtimeFirebase() {
+
+    FirebaseDatabase database = null;
+
+    try {
+      FirebaseOptions options = new FirebaseOptions.Builder()
+          .setCredentials(GoogleCredentials.getApplicationDefault())
+          .setDatabaseUrl("https://quizzy-step-2020.firebaseio.com/")
+          .build();
+
+      // Initialize DB with options
+      FirebaseApp.initializeApp(options);
+
+      // Reference to the whole Realtime DB
+      database = FirebaseDatabase.getInstance();
+
+      
+
+    } catch(IOException ie) {
+      ie.printStackTrace();
+    }
+
+    return database;
+    
   }
 }
