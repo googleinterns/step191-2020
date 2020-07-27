264ed921b599080ff502e9b68a16c9c18a7c35a8
Pamela <pamelalozano@google.com>
2020-07-27 21:57:47 +0000

    Servlet to active room and set questions

---
+++ src/main/java/com/google/sps/servlets/StartGame.java
@@ -0,0 +1,34 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.sps.daos.RoomDao;
+
+@WebServlet("/startGame")
+public class StartGame extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+
+    // Activate room
+    RoomDao dao = (RoomDao) this.getServletContext().getAttribute("roomDao");
+    dao.startGame();
+    //Todo: get room's game and get questions
+
+    response.getWriter().println(dao.getActualRoom());
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+  }
+
+}
