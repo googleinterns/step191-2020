3b17279fcc0eb8224d59900af3a1da3e5e5c976e
Casale <popobasura3@hotmail.com>
2020-07-28 17:05:40 -0500

    "Create Game" page created with it's servlet (#84)
    * Create game site created
    
    * Form parameters sent to NewGameServlet.java
    
    * auth.js file created only for auth Firebase stuff
    
    * Nitfixes
    
    * Scipts corrected
    
    * Scripts added to html
    
    * Merge conflicts resolved
    
    * Merge conflicts resolved
    
    * Update src/main/webapp/scripts/main.js
    
    Co-authored-by: Brett Peters <brettpeters@google.com>

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -88,36 +88,6 @@ function onCounterFormSubmit(e) {
   }
 }
 
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-function authStateObserver(user) {
-  if (user) { // User is signed in!
-    // Get the signed-in user's profile pic and name.
-    var profilePicUrl = getProfilePicUrl();
-    var userName = getUserName();
-
-    // Set the user's profile pic and name.
-    userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
-    userNameElement.textContent = userName;
-
-    // Show user's profile and sign-out button.
-    userNameElement.removeAttribute('hidden');
-    userPicElement.removeAttribute('hidden');
-    signOutButtonElement.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    signInButtonElement.setAttribute('hidden', 'true');
-
-  } else { // User is signed out!
-    // Hide user's profile and sign-out button.
-    userNameElement.setAttribute('hidden', 'true');
-    userPicElement.setAttribute('hidden', 'true');
-    signOutButtonElement.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    signInButtonElement.removeAttribute('hidden');
-  }
-}
-
 // Returns true if user is signed-in. Otherwise false and displays a message.
 function checkSignedInWithVote() {
   // Return true if the user is signed in Firebase
@@ -134,48 +104,13 @@ function checkSignedInWithVote() {
   return false;
 }
 
-
-// Adds a size to Google Profile pics URLs.
-function addSizeToGoogleProfilePic(url) {
-  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
-    return url + '?sz=150';
-  }
-  return url;
-}
-
-// A loading image URL.
-var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
-
-
-
-// Checks that the Firebase SDK has been correctly setup and configured.
-function checkSetup() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
-  }
-}
-
-// Checks that Firebase has been imported.
-checkSetup();
-
 // Shortcuts to DOM Elements.
 var counterFormElement = document.getElementById('counter-form');
 var submitButtonElement = document.getElementById('submit');
-var userPicElement = document.getElementById('user-pic');
-var userNameElement = document.getElementById('user-name');
-var signInButtonElement = document.getElementById('sign-in');
-var signOutButtonElement = document.getElementById('sign-out');
-var signInSnackbarElement = document.getElementById('must-signin-snackbar');
 
 // Saves vote on form submit.
 counterFormElement.addEventListener('submit', onCounterFormSubmit);
-signOutButtonElement.addEventListener('click', signOut);
-signInButtonElement.addEventListener('click', signIn);
 
-// initialize Firebase
-initFirebaseAuth();
 
 // We load currently existing votes and listen to new ones.
 loadCounter();
