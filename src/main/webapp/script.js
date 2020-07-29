69b642e605e8102b374ddab0248ec76043e0994f
Brett Peters <brettpeters@google.com>
2020-07-29 09:51:24 -0700

    Add jest testing (#87)
    * Add jest testing
    
    * newline
    
    * Update comment

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+    ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -26,3 +26,5 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+module.exports = { addRandomGreeting };
