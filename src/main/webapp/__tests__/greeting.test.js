011ea1304286c456a9dae8c3e9d8dacfa265205d
Brett Peters <brettpeters@google.com>
2020-07-28 17:07:18 -0700

    Add jest testing

---
+++ src/main/webapp/__tests__/greeting.test.js
@@ -0,0 +1,25 @@
+require('jest-extended'); // Provides more matchers like `toBeOneOf`
+const { addRandomGreeting } = require('script.js');
+
+// `describe` blocks can be used to group related tests together.
+describe('addRandomGreeting', () => {
+  // `test` is a single unit test
+  test('inserts a random greeting', () => {
+    // Set up our document body
+    document.body.innerHTML =
+      '<div id="greeting-container"></div>';
+
+    // Call the function under test
+    addRandomGreeting();
+
+    // Verify the result using matchers
+    const expectedGreetings = [
+      'Hello world!',
+      '¡Hola Mundo!',
+      '你好，世界！',
+      'Bonjour le monde!',
+    ];
+    const actualGreetingText = document.getElementById('greeting-container').innerText;
+    expect(actualGreetingText).toBeOneOf(expectedGreetings);
+  });
+});
