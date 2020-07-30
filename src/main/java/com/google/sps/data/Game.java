85942066868a76259284b2d52c3779eb67635487
thefredo1000 <popobasura3@hotmail.com>
2020-07-30 14:49:15 -0500

    Game class is now generated with Auto Value

--- src/main/java/com/google/sps/data/Game.java
+++ src/main/java/com/google/sps/data/Game.java
@@ -1,78 +1,27 @@
 package com.google.sps.data;
 
+import com.google.auto.value.AutoValue;
 import com.google.sps.data.Question;
 import java.util.List;
 
-public class Game {
+@AutoValue
+public abstract class Game {
 
-  private String creator;
-  private String title;
-  List<Question> questions;
-
-  public Game () {
-  }
-  /**
-   * Constructs a new game
-   * @param creator the id of the creator
-   * @param title the game title
-   * @param questions a list of questions
-   */
-  public Game(String creator, String title, List<Question> questions) {
-    this.title = title;  
-    this.creator = creator;  
-    this.questions = questions;
+  public static Builder builder() {
+    return new AutoValue_Game.Builder();
   }
 
-  /**
-   * @return the Game's title
-   */
-  public String getTitle() {
-    return this.title;  
-  }
-  /**
-   * Sets the game title
-   * @param title 
-   */
-  public void setTitle(String title) {
-    this.title = title;  
-  }
-  
-  /**
-   * @return the game's creator
-   */
-  public String getCreator() {
-    return this.title;  
-  }
+  public abstract String title();
+  public abstract String creator();
+  public abstract List<Question> questions();
 
-  /**
-   * Sets the game's creator
-   * @param creator 
-   */
-  public void setCreator(String creator) {
-    this.creator = creator;  
-  }
 
-  
-  /**
-   * @return the game's questions
-   */
-  public List<Question> getQuestions() {
-    return this.questions;
-  }
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder title(String title);
+    public abstract Builder creator(String creator);
+    public abstract Builder questions(List<Question> questions);
 
-  /**
-   * Sets the game's questions
-   * @param questions 
-   */
-  public void setQuestions(List<Question> questions) {
-    this.questions = questions;
-  }
-
-  /**
-   * Adds a question
-   * @param question
-   */
-  public void addQuestion(Question question) {
-    this.questions.add(question);
+    public abstract Game build();
   }
 }
