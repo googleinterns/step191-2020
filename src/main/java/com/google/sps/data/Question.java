9f74accbf9e35c62b31cddfe555b4252dec82747
thefredo1000 <popobasura3@hotmail.com>
2020-07-30 14:48:17 -0500

    (Nitfix) Answer class is now generated with Auto Value

--- src/main/java/com/google/sps/data/Question.java
+++ src/main/java/com/google/sps/data/Question.java
@@ -1,64 +1,26 @@
 package com.google.sps.data;
 
+import com.google.auto.value.AutoValue;
 import com.google.sps.data.Answer;
 import java.util.List;
 
-public class Question {
+@AutoValue
+public abstract class Question {
 
-  private String title; // The title of the question
-  List<Answer> answers; // A collection of answers
-
-  public Question () {
+  public static Builder builder() {
+    return new AutoValue_Question.Builder();
   }
 
-  /**
-   * Creates a new question
-   * @param title defines the title or the text that the question will have
-   * @param answers its a list of answers
-   */
-  public Question(String title, List<Answer> answers) {
-    this.title = title;
-    this.answers = answers;
+  public abstract String title();
+  public abstract List<Answer> answers();
+
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder title(String title);
+
+    public abstract Builder answers(List<Answer> answers);
+
+    public abstract Question build();
   }
-
-  /**
-   * @return the question title
-   */
-  public String getTitle() {
-    return this.title;  
-  }
-
-  /**
-   * Sets the title
-   * @param title defines the title or the text that the question will have
-   */
-  public void setTitle(String title) {
-    this.title = title;  
-  }
-
-  /**
-   * @return the answers
-   */
-  public List<Answer> getAnswers() {
-    return this.answers;  
-  }
-
-
-  /**
-   * Sets the answers
-   * @param answers its a list of answers
-   */
-  public void setAnswers(List<Answer> answer) {
-    this.answers = answers;
-  }
-
-
-  /**
-   * Adds one answer
-   * @param answer the asnwer that will be added
-   */
-  public void addAnswer(Answer answer) {
-    this.answers.add(answer);
-  }
-
 }
