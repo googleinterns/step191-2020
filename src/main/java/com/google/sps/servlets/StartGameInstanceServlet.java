8547932127abbb5b2f75747980bfd6507faeaed2
Pamela <pamelalozano@google.com>
2020-07-30 19:21:15 +0000

    Refactor error handling and return object json

--- src/main/java/com/google/sps/servlets/StartGameInstanceServlet.java
+++ src/main/java/com/google/sps/servlets/StartGameInstanceServlet.java
@@ -29,17 +29,22 @@ public class StartGameInstanceServlet extends HttpServlet {
 
     GameInstanceDao dao = (GameInstanceDao) this.getServletContext().getAttribute("gameInstanceDao");
     GameInstance newRoom = dao.getRoom(roomId);
-    // Activate room
-    if(newRoom != null){
-        newRoom.setIsActive(true);
-        dao.updateRoom(newRoom);
-        response.getWriter().println("Game started");
-    } else {
+
+    if(newRoom == null){
         response.setStatus(404);
         response.getWriter().println("Error, not found.");
+        return;
     }
+    
+    // Activate room
+    newRoom.setIsActive(true);
+    dao.updateRoom(newRoom);
 
-    //Todo: get room's game and get questions
+    //Return object
+    Gson gson = new Gson();
+    String json = gson.toJson(newRoom);
+    response.getWriter().println(json);
+
 
   }
 
