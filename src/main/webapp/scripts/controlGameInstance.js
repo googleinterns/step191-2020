3602890907d974922a07bc17c19471248e798db4
aroquev00 <aroquev@google.com>
2020-08-03 19:47:42 +0000

    Refactor and comment controlGameInstance.js

--- src/main/webapp/scripts/controlGameInstance.js
+++ src/main/webapp/scripts/controlGameInstance.js
@@ -17,27 +17,32 @@
 
 const db = firebase.firestore();
 let activeGameInstanceId;
-let game;
 
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 function authStateObserver(user) {
   if (user) { // User is signed in!
+    // Everything starts working when the User logs in
     getActiveGameInstanceId(user);
   } else { // User is signed out!
     console.log("Not logged in");
   }
 }
 
+// Is triggered when the User logs in or logs out
 function initAuthStateObserver() {
   firebase.auth().onAuthStateChanged(authStateObserver);
 }
 
+// Queries the "Users" collection of the DB to get the activeGameInstanceId the User is participating in
 function getActiveGameInstanceId(user) {
   const uid = user.uid;
 
+  // Query the User's document in "Users" collection
   db.collection("users").doc(uid).get().then(function(doc) {
     if (doc.exists) {
+      // Get the activeGameInstance's ID in which the User is participating
       activeGameInstanceId = doc.data().activeGameInstanceId;
+      // Query the info of that GameInstance from the "GameInstance" collections
       queryActiveGameInstanceDocument(activeGameInstanceId);
     } else {
         // doc.data() will be undefined in this case
@@ -48,13 +53,17 @@ function getActiveGameInstanceId(user) {
   });
 }
 
+// Gets the gameInstance entity from DB
 function queryActiveGameInstanceDocument(gameInstanceId) {
   db.collection("gameInstance").doc(gameInstanceId).get().then(function(doc) {
     if (doc.exists) {
-      // If this point is reached, a GameInstance extists
-      addGameInstanceIdToUI(gameInstanceId);
-      buildActiveGameInstanceUI(doc.data());
-      initUIButtons(gameInstanceId);
+      // The GameInstance exists, so now populate all info about it
+
+      // Add all the GameInstance's Info to UI
+      buildActiveGameInstanceUI(doc.data(), gameInstanceId);
+      
+      // Add buttons to control the GameInstance state
+      //initUIButtons(gameInstanceId);
     } else {
         // doc.data() will be undefined in this case
       console.log("No such document!");
@@ -64,22 +73,30 @@ function queryActiveGameInstanceDocument(gameInstanceId) {
   });
 }
 
+// Build the UI elements with all the info of the GameInstance
+function buildActiveGameInstanceUI(gameInstance, gameInstanceId) {
+  // Add the GameInstance's ID to UI
+  addGameInstanceIdToUI(gameInstanceId);
+
+  // Get the game object and add it to the UI
+  queryGameDetails(gameInstance.gameId);
+
+  // This will listen to when anything in the GameInstance changes
+  initGameInstanceListener();
+}
+
+// Adds the GameInstance's ID to the UI
 function addGameInstanceIdToUI(gameInstanceId) {
   const gameInstanceIdElement = document.getElementById("jsGameInstanceId");
-  gameInstanceIdElement.innerText = "This gameInstance's ID: " + gameInstanceId;
+  gameInstanceIdElement.innerText = "This gameInstance's ID is: " + gameInstanceId;
 }
 
-function buildActiveGameInstanceUI(gameInstance) {
-  queryGameDetails(gameInstance.gameId);
-}
-
+// Queries the Game instance from the DB and then adds it to the UI
 function queryGameDetails(gameId) {
   db.collection("games").doc(gameId).get().then(function(doc) {
     if (doc.exists) {
-      addGameIdToUI(gameId);
-      game = doc.data();
-      addGameDetailsToUI(game);
-      initGameInstanceListener();
+      const game = doc.data();
+      addGameDetailsToUI(game, gameId);
     } else {
         // doc.data() will be undefined in this case
       console.log("No such document!");
@@ -89,23 +106,31 @@ function queryGameDetails(gameId) {
   });
 }
 
+// Adds the Game's details to the UI
+function addGameDetailsToUI(game, gameId) {
+  // Adds the Game's ID to the UI
+  addGameIdToUI(gameId);
+
+  // Add the Game Title
+  const gameTitleElement = document.getElementById("jsGameTitle");
+  gameTitleElement.innerText = "The game's title is: " + game.title;
+
+  // Add the Number of Questions
+  const numberOfQuestionsOfGameElement = document.getElementById('jsNumberOfQuestionsOfGame');
+  numberOfQuestionsOfGameElement.innerText = 'This game has a total of ' + game.numberOfQuestions + ' questions.';
+}
+
+// Adds the Game's ID to the UI
 function addGameIdToUI(gameId) {
   const gameIdElement = document.getElementById("jsGameId");
   gameIdElement.innerText = "The game's ID is: " + gameId;
 }
 
-function addGameDetailsToUI(game) {
-  const gameTitleElement = document.getElementById("jsGameTitle");
-  gameTitleElement.innerText = "The game's title is: " + game.title;
-
-  const numberOfQuestionsOfGameElement = document.getElementById('jsNumberOfQuestionsOfGame');
-  numberOfQuestionsOfGameElement.innerText = 'This game has a total of ' + game.numberOfQuestions + ' questions.';
-}
-
 function initGameInstanceListener() {
   db.collection('gameInstance').doc(activeGameInstanceId).onSnapshot(function(doc) {
+    // If this is triggered it's because the GameInstance's activeQuestion changed
     const gameInstanceUpdate = doc.data();
-    updateGameInfo(gameInstanceUpdate.activeQuestionNumber);
+    updateGameInfo(gameInstanceUpdate.activeQuestionId);
     console.log(gameInstanceUpdate);
   });
 }
