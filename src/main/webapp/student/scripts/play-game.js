f9d26d5f75e4653663b8775ac15597f58af7c64a
aroquev00 <aroquev@google.com>
2020-08-04 03:25:30 +0000

    Check answers in a very bad way

--- src/main/webapp/student/scripts/play-game.js
+++ src/main/webapp/student/scripts/play-game.js
@@ -8,54 +8,80 @@ const urlParams = new URLSearchParams(queryString);
 const roomId = urlParams.get('room');
 var active = false;
 
-function checkIfActive(){
-    db.collection('gameInstance').doc(roomId).onSnapshot(function(doc) {
-        if(doc.data().isActive&&!active){
-            var readyHeading = document.getElementById("getReady");
-            var game = document.getElementById("gameSection");
-            readyHeading.classList.toggle("ready");
-            game.classList.toggle("active");
-            active = true;
-            listenGameInstance();
-        }
-    })
-}
-
-function listenGameInstance(){
-    db.collection('gameInstance').doc(roomId).onSnapshot(function(doc) {
-        getQuestion(doc.data().gameId, doc.data().currentQuestion);
-    })
-}
-
-function getQuestion(gameId, questionId){
-    const games = db.collection('games').doc(gameId);
-    games.collection('questions').doc(questionId).get().then(function(doc) {
-        if (doc.exists) {
-            createQuestionObject(doc.data().title);
-            createAnswersObject(doc.data().answers);
-        }
-    })
-}
-
-function createQuestionObject(title){
-    document.getElementById("question").innerText=title;
-}
-
-function createAnswersObject(answers){
-    const radioForm = document.getElementById("answerOptions");
-    document.getElementById("answerOptions").innerHTML="";
-    console.log(radioForm)
-    for(var i = 0; i < answers.length; i++){
-        var input = document.createElement("input"); 
-        input.setAttribute("type", "radio"); 
-        input.setAttribute("name", "answer"); 
-        input.setAttribute("value", answers[i].title); 
-        radioForm.appendChild(input);  
-        radioForm.append(answers[i].title);  
-        radioForm.appendChild(document.createElement("br")); 
+function checkIfActive() {
+  db.collection('gameInstance').doc(roomId).onSnapshot(function (doc) {
+    if (doc.data().isActive && !active) {
+      var readyHeading = document.getElementById("getReady");
+      var game = document.getElementById("gameSection");
+      readyHeading.classList.toggle("ready");
+      game.classList.toggle("active");
+      active = true;
+      listenGameInstance();
     }
+  })
 }
 
+function listenGameInstance() {
+  db.collection('gameInstance').doc(roomId).onSnapshot(function (doc) {
+    getQuestion(doc.data().gameId, doc.data().currentQuestion);
+  })
+}
+
+function getQuestion(gameId, questionId) {
+  const games = db.collection('games').doc(gameId);
+  games.collection('questions').doc(questionId).get().then(function (doc) {
+    if (doc.exists) {
+      createQuestionObject(doc.data().title);
+      createAnswersObject(doc.data().answers);
+      setSubmitButton(doc.data().answers);
+    }
+  })
+}
+
+function createQuestionObject(title) {
+  document.getElementById("question").innerText = title;
+}
+
+function createAnswersObject(answers) {
+  const radioForm = document.getElementById("answerOptions");
+  document.getElementById("answerOptions").innerHTML = "";
+  console.log(radioForm)
+  for (var i = 0; i < answers.length; i++) {
+    var input = document.createElement("input");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "answer");
+    input.setAttribute("value", answers[i].title);
+    if (answers[i].correct) {
+      input.id = "correctAnswer"
+    }
+    radioForm.appendChild(input);
+    radioForm.append(answers[i].title);
+    radioForm.appendChild(document.createElement("br"));
+  }
+
+  const answerFeedbackElement = document.getElementById("answerFeedback");
+  answerFeedbackElement.innerHTML = '';
+  answerFeedbackElement.className = '';
+
+
+  const answerFormElement = document.getElementById('answerForm');
+  answerFormElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const correctRadioInput = document.getElementById("correctAnswer");
+    answerFeedbackElement.className = '';
+    if (correctRadioInput.checked) {
+      answerFeedbackElement.innerText = "That is correct!"
+      answerFeedbackElement.classList.add('rightAnswer')
+    } else {
+      answerFeedbackElement.innerText = "Are you sure that's the correct answer?"
+      answerFeedbackElement.classList.add('wrongAnswer')
+    }
+  });
+}
+
+function setSubmitButton() {
+
+}
 
 checkIfActive();
 
