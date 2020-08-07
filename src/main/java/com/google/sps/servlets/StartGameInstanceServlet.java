070af11cdaaabf892c3630e87b2ae3dc09c9e33d
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2020-08-07 16:01:50 -0500

    Test for question control servlets (#108)
    * StartGameInstanceServlet Test
    
    Initialize firestore and tests
    
    Refactor get game with GameDao instead of query
    
    Test with Game object mock and GameDao mock
    
    Add headQuestion attribute
    
    Add getGame from db
    
    Verify values after update
    
    NoRoomId test
    
    Handle game not found error
    
    noGameInstanceFound and noGameFound tests
    
    noGameInstanceFound and noGameFound tests
    
    nit changes

--- src/main/java/com/google/sps/servlets/StartGameInstanceServlet.java
+++ src/main/java/com/google/sps/servlets/StartGameInstanceServlet.java
@@ -15,7 +15,9 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentSnapshot;
 
 import com.google.sps.daos.GameInstanceDao;
+import com.google.sps.daos.GameDao;
 import com.google.sps.data.GameInstance;
+import com.google.sps.data.Game;
 
 import java.util.List; 
 import java.util.ArrayList; 
@@ -28,7 +30,6 @@ public class StartGameInstanceServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
   // Generate room key
     String roomId = request.getParameter("gameInstance");
-    Firestore db = (Firestore) this.getServletContext().getAttribute("firestoreDb");
 
     if(roomId==null || roomId.isEmpty()){
         response.setStatus(500);
@@ -37,25 +38,25 @@ public class StartGameInstanceServlet extends HttpServlet {
     }
 
     GameInstanceDao dao = (GameInstanceDao) this.getServletContext().getAttribute("gameInstanceDao");
-    GameInstance newRoom = dao.getGameInstance(roomId);
-        
+    GameDao gameDao = (GameDao) this.getServletContext().getAttribute("gameDao");
+
+    GameInstance newRoom = dao.getGameInstance(roomId);       
     if(newRoom == null){
         response.setStatus(404);
-        response.getWriter().println("Error, not found.");
+        response.getWriter().println("Error, game instance not found.");
+        return;
+    }
+    
+    Game actualGame = gameDao.getGame(newRoom.getGameId());
+    if(actualGame == null){
+        response.setStatus(404);
+        response.getWriter().println("Error, game not found.");
         return;
     }
 
-        ApiFuture<DocumentSnapshot> future = db.collection("games").document(newRoom.getGameId()).get();
-        try {
-            DocumentSnapshot document = future.get();
-            newRoom.setCurrentQuestion(document.get("headQuestion").toString());
-        } catch(Exception e) {
-            System.out.println(e);
-            return;
-        }
-
-        // Activate room
-        newRoom.setIsActive(true);
+    newRoom.setCurrentQuestion(actualGame.headQuestion());
+    // Activate room
+    newRoom.setIsActive(true);
 
 
     dao.updateGameInstance(newRoom);
