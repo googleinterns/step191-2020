7883b427cb29e58470f84996cdc3bb4be60efd8b
thefredo1000 <popobasura3@hotmail.com>
2020-08-13 14:36:18 -0500

    Nitfix/Merge conflicts resolved

--- src/main/java/com/google/sps/servlets/NewGameServlet.java
+++ src/main/java/com/google/sps/servlets/NewGameServlet.java
@@ -44,7 +44,7 @@ public class NewGameServlet extends HttpServlet {
     String gameJsonStr = request.getParameter("game");
 
     // We create the game object
-    Game currentGame = Game.buildWithJson(gameJsonStr);
+    Game currentGame = buildGameWithJson(gameJsonStr);
     
 
     GameDao dao = (GameDao) this.getServletContext().getAttribute("gameDao");
@@ -52,4 +52,51 @@ public class NewGameServlet extends HttpServlet {
 
     response.sendRedirect("/teacher/createGame.html");
   }
+
+  public Game buildGameWithJson(String gameJsonStr) {
+    // The function recieves the JSON and converts to a JsonObject
+    String gameJson = gameJsonStr;
+    JsonElement gameJsonElem = new JsonParser().parse(gameJson);
+    JsonObject gameJsonObj = gameJsonElem.getAsJsonObject();
+
+    // We retrieve the attributes from the game
+    String gameTitle = gameJsonObj.get("title").getAsString();
+    String gameCreator = gameJsonObj.get("creator").getAsString();
+
+    List<Question> questions = new ArrayList(Arrays.asList()); // Here we will store the questions as objects
+
+    // We get the questions as a JsonArray and then we iterate to add them to the list
+    JsonArray questionsJsonArray = (JsonArray) gameJsonObj.get("questions");
+
+    for ( int i = 0; i < questionsJsonArray.size(); i++ ) {
+      JsonObject questionJsonObj = (JsonObject) questionsJsonArray.get(i);
+
+      // We retrieve the attributes from the question
+      String questionTitle = questionJsonObj.get("title").getAsString();
+      boolean isMC = questionJsonObj.get("isMC").getAsBoolean();
+
+      List<Answer> answers = new ArrayList(Arrays.asList()); // Here we will store the answers as objects
+
+      // We get the answers as a JsonArray and then we iterate to add them to the list
+      JsonArray answersJsonArray = (JsonArray) questionJsonObj.get("answers");
+
+      for ( int j = 0; j < answersJsonArray.size(); j++ ) {
+        JsonObject answerJsonObj = (JsonObject) answersJsonArray.get(j);
+      
+        // We retrieve the attributes from the answer
+        String answerTitle = answerJsonObj.get("title").getAsString();
+        boolean isCorrect = answerJsonObj.get("correct").getAsBoolean();
+
+        // We create a new answer object with the attributes that we recovered then add the object to the list
+        answers.add(new Answer(answerTitle, isCorrect));
+      }
+      // We create a new question object with the attributes that we recovered and the answers, then we add the object to the list
+      questions.add(new Question(questionTitle, answers, isMC));
+    }
+
+    // We return the game object
+  return Game.builder().title(gameTitle).creator(gameCreator).questions(questions).headQuestion(questions.get(0).getId()).build(); 
+    
+  }
+
 }
